92bff08e3318968eda88cf0136286bf41edecdb0
BenWong1097 <ben.wong.jamin@gmail.com>
2019-04-10 22:39:33 +0000

    /orders path will now show the Order's json and ping the customer's phone with a Twilio text

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,23 +20,35 @@ class OrdersController < ApplicationController
   #       not want this to be done, use the above show method and
   #       specify the ID in your request
   def index
+    twilio_client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+    message = 'Come over, swipe your card, and pick up your order!'
     @order = Order.where(completed: false).first
     unless @order.nil?
       @order.update_attributes!(completed: true)
     end
+    if @order.nil? or @order.drinks.nil?
+      render json: ""
+      return
+    end
+    @recipes = Array.new
+    @order.drinks.each do |drink|
+      recipe = Hash.new
+      drink.ingredients.each do |ingredient|
+        recipe[ingredient.name] = ingredient.amount
+      end
+      @recipes << recipe
+    end
+    render json: @recipes
     
-    respond_to do |format|
-      format.html
-      format.json {
-        @recipes = Array.new
-        
-        render json: @recipes and return if @order.nil?
-        
-        @order.drinks.each do |drink|
-          @recipes << drink.ingredients
-        end
-        render json: @recipes
-      }
+    user = @order.user
+    phone_number = ''
+    if not user.phone_number.empty?
+      if ! user.phone_number.include? "+1"
+        phone_number = '+1'
+      end
+      phone_number << user.phone_number
+      twilio_client.messages.create from: Rails.application.secrets.twilio_phone_number, to: phone_number, body: message
+      puts "\e[32m(" + phone_number + ") Sent message: " + message + "\e[0m"
     end
   end
 end
